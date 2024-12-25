4544f13116a7d8725eb48ed5d33155914ea82223
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-25 22:28:31 +0530

    Stock Management made

---
+++ src/Stock.java
@@ -0,0 +1,15 @@
+public class Stock {
+    String name;
+    int numberOfShares;
+    double sharePrice;
+
+    Stock(String name, int numberOfShares, double sharePrice) {
+        this.name = name;
+        this.numberOfShares = numberOfShares;
+        this.sharePrice = sharePrice;
+    }
+
+    double getStockValue() {
+        return numberOfShares * sharePrice;
+    }
+}
